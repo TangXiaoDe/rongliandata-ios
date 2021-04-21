08da0c91de92f7789e7fbf514faf238c65876e76
TangXiaoDe <xiaodehappy@foxmail.com>
2021-04-21 15:04:01 +0800

    Equipment: 设备中锁仓详情列表进度天数显示更新

--- AntClusterB/Source/Module/Equipment/LockDetail/Model/LockDetailListModel.swift
+++ AntClusterB/Source/Module/Equipment/LockDetail/Model/LockDetailListModel.swift
@@ -52,16 +52,15 @@ class LockDetailLogModel: Mappable {
 
     
     var days: Int {
-        let strToday: String = Date.init().string(format: "yyyy-MM-dd", timeZone: .current)
-        let today = Date.dateWithString(strToday + " 00:00:00", format: "yyyy-MM-dd HH:mm:ss", timeZone: .current)
-        let strCreateDay: String = self.created_at.string(format: "yyyy-MM-dd", timeZone: .current)
-        let createDay = Date.dateWithString(strCreateDay + " 00:00:00", format: "yyyy-MM-dd HH:mm:ss", timeZone: .current)
-        
+        // 线性释放天数计算方法：(当前时间  - 记录时间（记录日期 + 23:30） - 1)  向下取整
+        let nowDate: Date = Date.init()
+        let strCreateDay: String = self.created_at.string(format: "yyyy-MM-dd 23:30:00", timeZone: .current)
+        let createDay = Date.dateWithString(strCreateDay, format: "yyyy-MM-dd HH:mm:ss", timeZone: .current)
         var days: Int = 0
-        if let today = today, let createDay = createDay {
-            days = Int(today.timeIntervalSince(createDay) / (24.0 * 60.0 * 60.0))
+        if let createDay = createDay {
+            days = Int(nowDate.timeIntervalSince(createDay) / (24.0 * 60.0 * 60.0)) - 1
         } else {
-            days = Int(Date.init().timeIntervalSince(self.created_at) / (24.0 * 60.0 * 60.0))
+            days = Int(Date.init().timeIntervalSince(self.created_at) / (24.0 * 60.0 * 60.0)) - 1
         }
         days = min(180, max(0, days))
         return days
