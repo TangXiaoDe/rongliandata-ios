f69d1b1ee9440b5e5c6c79870ef6581b1111523d
zhaowei <1009105480@qq.com>
2021-04-25 16:50:31 +0800

    Equipment: 设备锁仓详情流水中释放天数更新

--- AntClusterB/Source/Module/Equipment/LockDetail/Model/LockDetailListModel.swift
+++ AntClusterB/Source/Module/Equipment/LockDetail/Model/LockDetailListModel.swift
@@ -52,13 +52,26 @@ class LockDetailLogModel: Mappable {
 
     
     var days: Int {
-        // 线性释放天数计算方法：(当前时间  - 记录时间（记录日期 + 23:30） - 1)  向下取整
-        let nowDate: Date = Date.init()
-        let strCreateDay: String = self.created_at.string(format: "yyyy-MM-dd 23:30:00", timeZone: .current)
+//        // 线性释放天数计算方法：(当前时间  - 记录时间（记录日期 + 23:30） - 1)  向下取整
+//        let nowDate: Date = Date.init()
+//        let strCreateDay: String = self.created_at.string(format: "yyyy-MM-dd 23:30:00", timeZone: .current)
+//        let createDay = Date.dateWithString(strCreateDay, format: "yyyy-MM-dd HH:mm:ss", timeZone: .current)
+//        var days: Int = 0
+//        if let createDay = createDay {
+//            days = Int(nowDate.timeIntervalSince(createDay) / (24.0 * 60.0 * 60.0)) - 1
+//        } else {
+//            days = Int(Date.init().timeIntervalSince(self.created_at) / (24.0 * 60.0 * 60.0)) - 1
+//        }
+//        days = min(180, max(0, days))
+//        return days
+        // 线性释放天数计算方法：(当前日期  - 记录日期 - 1)  向下取整
+        let strToday: String = Date.init().string(format: "yyyy-MM-dd 00:00:00", timeZone: .current)
+        let today = Date.dateWithString(strToday, format: "yyyy-MM-dd HH:mm:ss", timeZone: .current)
+        let strCreateDay: String = self.created_at.string(format: "yyyy-MM-dd 00:00:00", timeZone: .current)
         let createDay = Date.dateWithString(strCreateDay, format: "yyyy-MM-dd HH:mm:ss", timeZone: .current)
         var days: Int = 0
-        if let createDay = createDay {
-            days = Int(nowDate.timeIntervalSince(createDay) / (24.0 * 60.0 * 60.0)) - 1
+        if let today = today, let createDay = createDay {
+            days = Int(today.timeIntervalSince(createDay) / (24.0 * 60.0 * 60.0)) - 1
         } else {
             days = Int(Date.init().timeIntervalSince(self.created_at) / (24.0 * 60.0 * 60.0)) - 1
         }
