3c28bb0cb818f53a96c1ec579938a8190b907c6f
zhaowei <1009105480@qq.com>
2021-06-28 18:14:18 +0800

    Brand:添加接口和模型

--- AntClusterB/Source/Module/Mine/Brand/Model/BrandModel.swift
+++ AntClusterB/Source/Module/Mine/Brand/Model/BrandModel.swift
@@ -8,3 +8,36 @@
 //  品牌商模型
 
 import Foundation
+import ObjectMapper
+
+class BrandModel: Mappable {
+    var id: Int = 0
+    /// 品牌商名称
+    var name: String = ""
+    /// 品牌商头像
+    var avatar: String = ""
+    /// fil采购总量
+    var fil: Double = 0
+    /// xch采购总量
+    var xch: Double = 0
+    /// bzz节点数
+    var bzz: Double = 0
+
+    /// 头像url
+    var avatarUrl: URL? {
+        return UrlManager.fileUrl(name: self.avatar)
+    }
+
+    required init?(map: Map) {
+
+    }
+    func mapping(map: Map) {
+        id <- map["id"]
+        name <- map["name"]
+        avatar <- map["avatar"]
+        fil <- (map["fil"], DoubleStringTransform.default)
+        xch <- (map["xch"], DoubleStringTransform.default)
+        bzz <- (map["bzz"], DoubleStringTransform.default)
+    }
+}
+
